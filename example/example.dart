a12ad4e39ac678ba50239ac2807a6a535d6b325e
Mouaad Aallam <mouaad@aallam.com>
2022-08-19 16:30:15 +0200

    chore: add example

---
+++ example/example.dart
@@ -0,0 +1,21 @@
+import 'package:ulid4d/ulid.dart';
+
+void main() {
+  // generate a ULID String
+  final ulidString = ULID.randomULID();
+
+  // generate a ULID instance
+  final ulid = ULID.nextULID();
+
+  // generate a byte array (Uint8List) a ULID instance
+  final data = ulid.toBytes();
+
+  // generate a ULID from given byte array using 'fromBytes'
+  final ulidFromBytes = ULID.fromBytes(data);
+
+  // generate a ULID from given String using 'fromString'
+  final ulidFromString = ULID.fromString(ulidString);
+
+  // generate a ULID String from ULID instance
+  final ulidStringFromULID = ulid.toString();
+}
