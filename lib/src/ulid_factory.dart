50b16731ef6b27c073fbbe531d899443c34f846e
Mouaad Aallam <mouaad@aallam.com>
2022-09-02 11:29:21 +0200

    split ulid factory class

--- lib/src/ulid_factory.dart
+++ lib/src/ulid_factory.dart
@@ -8,41 +8,56 @@ import 'ulid.dart';
 import 'utils.dart';
 
 /// [ULID] factory builder.
-class ULIDFactory {
+abstract class ULIDFactory {
   /// Creates a [ULIDFactory] instance, with optional [random] generator.
-  factory ULIDFactory([Random? random]) => ULIDFactory._(random ?? Random());
-
-  /// Default [ULIDFactory] constructor.
-  const ULIDFactory._(this._random);
-
-  /// Internal random generator.
-  final Random _random;
+  factory ULIDFactory([Random? random]) => _ULIDFactory(random ?? Random());
 
   /// Generate a ULID String.
+  String randomULID([int? timestamp]);
+
+  /// Generate a [ULID].
+  ULID nextULID([int? timestamp]);
+
+  /// Generate a [ULID] from given bytes.
+  /// [data] must be 16 bytes in length.
+  ULID fromBytes(Uint8List data);
+
+  /// Create [ULID] object from given (valid) ULID [string].
+  ULID fromString(String string);
+}
+
+/// Default implementation of [ULIDFactory].
+class _ULIDFactory implements ULIDFactory {
+  /// Creates [_ULIDFactory] instance.
+  const _ULIDFactory(this.random);
+
+  /// Internal random generator.
+  final Random random;
+
+  @override
   String randomULID([int? timestamp]) {
     final time = timestampOrCurrent(timestamp);
     requireTimestamp(time);
     final buffer = Uint8List(26)
       ..write(time, 10, 0)
-      ..write(_random.nextInt(max32bit).toInt64(), 8, 10)
-      ..write(_random.nextInt(max32bit).toInt64(), 8, 18);
+      ..write(random.nextInt(max32bit).toInt64(), 8, 10)
+      ..write(random.nextInt(max32bit).toInt64(), 8, 18);
     return String.fromCharCodes(buffer);
   }
 
-  /// Generate a [ULID].
+  @override
   ULID nextULID([int? timestamp]) {
     final time = timestampOrCurrent(timestamp);
     requireTimestamp(time);
-    var mostSignificantBits = _random.nextInt(max32bit).toInt64();
-    final leastSignificantBits = _random.nextInt(max32bit).toInt64();
+    var mostSignificantBits = random.nextInt(max32bit).toInt64();
+    final leastSignificantBits = random.nextInt(max32bit).toInt64();
     mostSignificantBits = mostSignificantBits & mask16Bits; // random 16 bits
     mostSignificantBits =
         mostSignificantBits | (time << 16); // timestamp (32+16) + 16 random
     return DefaultULID(mostSignificantBits, leastSignificantBits);
   }
 
-  /// Generate a [ULID] from given bytes.
-  /// [data] must be 16 bytes in length.
+  @override
   ULID fromBytes(Uint8List data) {
     require(data.length == 16, 'data must be 16 bytes in length');
     var mostSignificantBits = Int64.ZERO;
@@ -57,7 +72,7 @@ class ULIDFactory {
     return DefaultULID(mostSignificantBits, leastSignificantBits);
   }
 
-  /// Create [ULID] object from given (valid) ULID [string].
+  @override
   ULID fromString(String string) {
     require(string.length == 26, 'ULID string must be exactly 26 chars long');
 
